6f4eb84c8d4983c1fe657c80b971671c7ee62fcb
Panya <panya.rattanarom@sky.uk>
2025-09-29 11:20:39 +0100

    CREATE make decision blocks

--- src/frontend/components/DecisionBlock.jsx
+++ src/frontend/components/DecisionBlock.jsx
@@ -1,7 +1,22 @@
 import React from "react";
 
+const decisions = ["rock", "paper", "scissors"];
+
 const DecisionBlock = () => {
-  return <div>DecisionBlock</div>;
+  return (
+    <div className="flex flex-row gap-5 ">
+      {decisions.map((item, index) => {
+        return (
+          <div
+            key={index}
+            className="min-w-sm rounded overflow-hidden shadow-lg text-center"
+          >
+            {item}
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default DecisionBlock;
